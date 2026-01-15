de72467ea6e900ef6ef4e31384ed447c69be0d2d
Klejnos <goklejnos@gmail.com>
2026-01-15 10:33:43 +0100

    Add files via upload

---
+++ Dzial_5/Przykład 23/main.cpp
@@ -0,0 +1,12 @@
+#include <windows.h> 
+#include <iostream> 
+#include <conio.h> 
+#include <cstdlib> 
+using namespace std; 
+int main() 
+{ 
+char hej[5]={'A', 'h', 'o', 'j'};  
+cout << hej;                           
+//program wyœwietli nam zawartoœæ ca³ej tablicy hej 
+getch(); 
+} 
